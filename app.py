510f526144a93879986e30d9252bb95db6722f85
cmonbruges <angelo-dc@hotmail.com>
2025-08-01 17:59:38 +0200

    app.py

--- app.py
+++ app.py
@@ -72,5 +72,8 @@ def manual_check():
     return jsonify({'new_items': new_items})
 
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
